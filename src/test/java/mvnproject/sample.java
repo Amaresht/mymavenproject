a4ecc1ef4f97398bd812cb993a7fa5ca78f00460
Amaresh <amaresh.tester@gmail.com>
2017-05-15 17:21:12 +0530

    initialupload

---
+++ src/test/java/mvnproject/sample.java
@@ -0,0 +1,11 @@
+package mvnproject;
+
+import org.testng.annotations.Test;
+
+public class sample {
+	@Test
+	public void s(){
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/mvnproject/sample1.java
@@ -0,0 +1,11 @@
+package mvnproject;
+
+import org.testng.annotations.Test;
+
+public class sample1 {
+	@Test
+	public void sam(){
+		System.out.println("hello sam");
+	}
+
+}
